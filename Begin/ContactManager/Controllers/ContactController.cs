ab60610d82264421302f6555eb450ac00d8405e9
mbrica <marko.brica@gmail.com>
2016-04-22 10:58:23 +0200

    Zadatak 1

---
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using ContactManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ContactManager.Services;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        } 
+        public Contact[] Get()
+        {
+            return this.contactRepository.GetAllContacts();
+    {
+        new Contact
+        {
+            Id = 1,
+            Name = "Glenn Block"
+        };
+        new Contact
+        {
+            Id = 2,
+            Name = "Dan Roth"
+        };
+    }
+        }
+    }
+}
